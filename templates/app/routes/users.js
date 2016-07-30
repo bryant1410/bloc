1f6aa87972bc877e0f6b9126acd447d716cf3258
kejace <kejace@gmail.com>
2016-07-29 22:37:34 -0400

    /import works, still needs more documentation

--- templates/app/routes/users.js
+++ templates/app/routes/users.js
@@ -304,11 +304,11 @@ router.post('/:user/:address/contract', cors(), function(req, res) {
           } else {
             console.log("caught a multi-contract")
             console.log("uploading " + contract)
-            return upload(contract,privkeyFrom, args);
+            return upload(contract, privkeyFrom, args);
           }
           
         }).then(function (arr) {
-          console.log(arr[3]);
+          console.log("address of contract: " + arr[3]);
           res.send(arr[3]);
         }).catch(function(_) {
           res.send("error uploading contract");
@@ -332,6 +332,9 @@ router.post('/:user/:address/import', jsonParser, cors(), function(req, res) {
   var address = req.params.address;
   var user = req.params.user;
 
+  var name = req.body.name;
+  var contract = req.body.contract;
+
   var args = req.body.args || {};
   console.log("constructor arguments: " + JSON.stringify(req.body.args));
 
@@ -340,7 +343,7 @@ router.post('/:user/:address/import', jsonParser, cors(), function(req, res) {
 
     if (data.addresses[0] == address) {
       console.log("user address found");
-      found = true; cb(null,data); 
+      cb(null,data); 
     }
     else{
       console.log("address does not exist for user");
@@ -366,8 +369,9 @@ router.post('/:user/:address/import', jsonParser, cors(), function(req, res) {
     console.log("src: " + JSON.stringify(src))
     
     api.Solidity(src)
-    .then(function(solObj) { 
-      console.log("have solidity object")
+    .then(function(solObjs) { 
+      var solObj = solObjs[contract][name]
+      console.log("have solidity object: " + JSON.stringify(solObj))
       var toret;
       if (args.constructor === Object) {
         console.log("calling constructor")
@@ -377,11 +381,11 @@ router.post('/:user/:address/import', jsonParser, cors(), function(req, res) {
         console.log("calling constructor(2)")
         toret = solObj.construct.apply(solObj, args);
       }
-      return toret.callFrom(privkeyFrom);  
+      return toret.callFrom(privkeyFrom);
     })
     .then(function (arr) {
-      console.log("hm: " + arr[3]);
-      res.send(arr[3]);
+      console.log("address of imported contract: " + arr.account.address);
+      res.send(arr.account.address.toString());
     }).catch(function(e) {
       res.send("error uploading contract: " + e);
     });
@@ -400,8 +404,8 @@ router.post('/:user/:address/import', jsonParser, cors(), function(req, res) {
      method: theMethod,
      args: {
         namedArg1: val1,
-  namedArg2: val2,
-  ..
+        namedArg2: val2,
+        ..
         }
     }
 */
