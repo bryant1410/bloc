eb7250f2ce4ed2a0fb00ed3f3bb61709aa607e58
Charles Crain <charles@blockapps.net>
2016-08-29 00:16:22 -0400

    completed reduced route implementation

--- templates/app/routes/search.js
+++ templates/app/routes/search.js
@@ -155,4 +155,109 @@ router.get('/:contractName/state', cors(), function (req, res) {
     }
 });
 
+router.get('/:contractName/state/reduced', cors(), function (req, res) {
+  const reducedStatePropeties = ['currentVendor', 'sampleType', 'currentState',
+    'currentLocationType','buid', 'wellName'];
+  var contractName = req.params.contractName;
+  var found = false;
+
+  var addresses;
+  var states = {};
+  var promises = [];
+  var masterContract = {};
+  var xabi = {};
+
+
+  let results = helper.contractsMetaAddressStream(contractName, 'Latest');
+
+  if(results === null){
+    console.log("couldn't find any contracts");
+    res.send("[]")
+  } else {
+      results.pipe( es.map(function (data,cb) {
+        if (data.name == contractName) {
+          found = true;
+          masterContract = JSON.stringify(data);
+          xabi = data.xabi;
+          cb(null,data);
+        }
+         else cb();
+      }))
+
+      .pipe( es.map(function (data, cb) {
+        rp({uri: apiURI + '/eth/v1.2/account?address='+data.address, json: true})
+          .then(function (result) {
+            //console.log("s1: " + JSON.stringify(result))
+            cb(null, result[0].code)
+          })
+          .catch(function (err) {
+            cb(null, err)
+          });
+      }))
+
+      .pipe( es.map(function (data, cb) {
+        rp({uri: apiURI + '/eth/v1.2/account?code='+data, json: true})
+          .then(function (result) {
+            cb(null, result)
+          })
+          .catch(function (err) {
+            console.log("rp failure", err);
+            cb(null, err)
+          });
+      }))
+
+      .pipe( es.map(function (data,cb) {
+        addresses = data.map(function (item) {
+          return item.address;
+        });
+        cb(null,addresses);
+      }))
+
+      .on('data', function(data) {
+        let items = data;
+        let contractData = {};
+
+        //Get initial abi/bin to create first contract
+        for(var prop in masterContract){
+          contractData[prop] = masterContract[prop];
+        }
+        for(var i=0; i < items.length; i++) {
+          const item = items[i];
+          const contractData = JSON.parse(masterContract);
+
+          const contract = Solidity.attach(contractData);
+
+          var promise = Promise.props(contract.state).then(function(sVars) {
+            var reduced = {};
+             reducedStatePropeties.forEach(function(prop) {
+              reduced[prop] = sVars[prop];
+            });
+            var parsed = traverse(reduced).forEach(function (x) {
+              if (Buffer.isBuffer(x)) {
+                this.update(x.toString());
+              }
+            });
+            return parsed;
+          })
+          .catch(function(err) {
+            console.log("contract/state sVars - error: " + err)
+          });
+          promises.push(promise);
+        }
+      })
+
+      .on('end', function () {
+
+        if (!found) {
+          res.send("contract not found");
+        }
+        else {
+          Promise.all(promises).then(function(resp){
+            res.send(resp);
+          });
+        }
+      });
+    }
+});
+
 module.exports = router;
