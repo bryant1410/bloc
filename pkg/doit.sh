1a4e11b8c020b6235c98c700b20452f66f4a734f
Ryan Reich <ryan@blockapps.net>
2016-10-24 19:10:07 +0000

    Fix fqdn and change http to https in doit.sh

--- pkg/doit.sh
+++ pkg/doit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-blochost=$fqdn
+blochost=$(curl ident.me)
 
 cd /var/run/strato/bloc-server
 blocserver="/usr/lib/strato/bloc-server/bin/main.js"
-sed -i "s|^apiURL: .*\$|apiURL: 'http://$blochost'|" config.yaml
+sed -i "s|^apiURL: .*\$|apiURL: 'https://$blochost'|" config.yaml
 HOST=0.0.0.0 exec $blocserver start
