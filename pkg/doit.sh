32b0186c74b0f14e5144229b5672f09d1c05c083
Ryan Reich <ryan@blockapps.net>
2016-11-11 17:03:20 +0000

    Make the strato host configurable

--- pkg/doit.sh
+++ pkg/doit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-blochost=$(curl ident.me)
+stratoHost=${stratoHost:-$(curl ident.me)}
 
 cd /var/run/strato/bloc-server
 blocserver="/usr/lib/strato/bloc-server/bin/main.js"
-sed -i "s|^apiURL: .*\$|apiURL: 'http$(${ssl:-false} && echo "s")://$blochost/strato-api'|" config.yaml
+sed -i "s|^apiURL: .*\$|apiURL: 'http$(${ssl:-false} && echo "s")://$stratoHost/strato-api'|" config.yaml
 HOST=0.0.0.0 exec $blocserver start
