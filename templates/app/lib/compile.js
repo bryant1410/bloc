aba8de395e6671817f78e98fd684dc61ab5e7446
Charles Crain <charles@blockapps.net>
2016-11-22 21:10:55 -0800

    compile was failing to index multiple contracts

--- templates/app/lib/compile.js
+++ templates/app/lib/compile.js
@@ -27,7 +27,7 @@ function compileSol(solSrc) {
       dirs.push(path.join('app','meta', solObj.name));
     }
 
-    console.log(chalk.yellow("Compile successful: " + solSrc));
+    // console.log(chalk.yellow("Compile successful: " + solSrc));
 
     var theObj = {};
 
@@ -42,9 +42,11 @@ function compileSol(solSrc) {
       innerObj[name] = solObj;
       theObj['src'] = innerObj;
     }
-
     dirs.map(function(contractPath) {
       mkdirp.sync(contractPath);
+
+      //use this to prevent unnecessary posts to create table
+      var contractNameInPath = contractPath.slice(contractPath.lastIndexOf('/')+1)
       for (contractName in theObj.src) {
         var contract = theObj.src[contractName];
         var multiPath = path.join(contractPath, contractName + '.json');
@@ -55,14 +57,10 @@ function compileSol(solSrc) {
         console.log(chalk.green("wrote: ") + multiPath);
 
         if(solSrc.searchable) {
-          console.log('searchable exists');
           var detached = contract.detach();
           for(var i=0; i < solSrc.searchable.length; i++){
-            console.log('solSrc.searchable[i]' + i, solSrc.searchable[i],
-              'detached.name', detached.name);
-            if(solSrc.searchable[i] === contractName) {
-              console.log('found a match');
-
+            // only if writing to folder of contractNameInPath do we attempt to make the table
+            if(solSrc.searchable[i] === contractNameInPath && solSrc.searchable[i] === contractName) {
               //BEWARE: removing strato-api from apiUrl. Likely need a cirrusUrl
               //field in config.yaml.
               var apiUrl = yamlConfig.readYaml('config.yaml').apiURL;
@@ -75,32 +73,15 @@ function compileSol(solSrc) {
                   'Content-Type': 'application/json'
                 }
               };
-                // json: true
               rp(options).then(function(_){
-                console.log('Successfully created table in cirrus for contract ' + contractName);
+                console.log('Successfully created table in cirrus for contract ');
               })
               .catch(function(err){
                 console.log('Error Creating table in cirrus: ', err);
               });
             }
-            break;
           }
         }
-        // var options = {
-        //   method: 'POST',
-        //   uri: yamlConfig.readYaml('config.yaml').apiURL + ':3333',
-        //   body: contract.detach(),
-        //   headers: {
-        //     'Content-Type': 'application/json'
-        //   }
-        // };
-        //   // json: true
-        // rp(options).then(function(_){
-        //   console.log('Successfully created table in cirrus for contract ' + contractName);
-        // })
-        // .catch(function(err){
-        //   console.log('Error Creating table in cirrus: ', err);
-        // });
       }
     });
     return theObj;
